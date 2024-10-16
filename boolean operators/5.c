36eee1ee169b30922ce6206639644773a5971ce6
Rohan1116 <sairohanmadamshetty@gmail.com>
2024-10-16 09:45:32 +0530

    Update 5.c

--- boolean operators/5.c
+++ boolean operators/5.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  int myAge = 28;
+  int myAge = 8;
   int votingAge = 18;
-  
+
   printf("%d", myAge >= votingAge);
-  if(myAge >= votingAge){
+  if(myAge >= votingAge)
     printf("\nYou are eligible for voting.");
-  }
-  else(myAge < votingAge);{
-    printf("You are not eligible for voting.");
-  }
+  else if(myAge < votingAge)
+    printf("\nYou are not eligible for voting.");
   return 0;
 }
